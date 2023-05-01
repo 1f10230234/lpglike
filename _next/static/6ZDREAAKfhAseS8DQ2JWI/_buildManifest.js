fa95e6a738d6fa2c98b12e7ce23e7014d2e16fa9
1f10230234 <1f10230234@users.noreply.github.com>
2023-05-01 15:49:51 +0000

    deploy: e43e1ef9cef670d777b8434cc25e9ba1ddf1dbe1

--- _next/static/6ZDREAAKfhAseS8DQ2JWI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4b676378455309d6.css","static/chunks/pages/index-df749a3d1207ec64.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/8Yj3ruthlNFF3Etrtu8Vk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4b676378455309d6.css","static/chunks/pages/index-5e2c185ff6696619.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
