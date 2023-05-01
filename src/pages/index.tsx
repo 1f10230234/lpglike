50d79d66080993ca97bb8b714c771f9f6f90f2db
ito yossuli <sif102302348@iniad.org>
2023-05-01 23:23:28 +0900

    最初の制作物（タートルアニメーション）アニメーション）

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,14 +28,14 @@ const Home = () => {
   let countBrack = 0;
   let countWhite = 0;
   console.log('a');
+  let isPlace = false;
+  let isPredict = false;
   const clickMasu = (x: number, y: number) => {
-    let isPlace = false;
-    let isPredict = false;
     if (board.some((row) => row.includes(3))) {
       if (board[y][x] === 3) {
-        ////console.log(x, y);
+        console.log(x, y);
         for (const n of direction) {
-          ////console.log(n);
+          console.log(n);
           for (
             let distance = 1;
             distance <=
@@ -45,42 +45,42 @@ const Home = () => {
             );
             distance++
           ) {
-            //console.log('!');
+            console.log('!');
             if (board[y + n[1] * distance][x + n[0] * distance] % 3 === 0) {
-              //console.log('a');
+              console.log('a');
               distance = 0;
               break;
             } else if (board[y + n[1] * distance][x + n[0] * distance] === 3 - turnColor) {
-              //console.log('b');
+              console.log('b');
               continue;
             } else if (board[y + n[1] * distance][x + n[0] * distance] === turnColor) {
-              //console.log('c');
-              ////console.log('d', distance);
+              console.log('c');
+              console.log('d', distance);
               for (let i = 1; i < distance; i++) {
                 newBoard[y + n[1] * (distance - i)][x + n[0] * (distance - i)] = turnColor;
                 isPlace = true;
               }
-              ////console.log(isPlace);
+              console.log(isPlace);
               break;
             }
           }
         }
         if (isPlace) {
-          //console.log('e');
+          console.log('e');
           newBoard[y][x] = turnColor;
           for (let x1 = 0; x1 < 8; x1++) {
             for (let y1 = 0; y1 < 8; y1++) {
-              //console.log(newBoard[y1][x1]);
+              console.log(newBoard[y1][x1]);
               newBoard[y1][x1] %= 3;
             }
           }
           //console.table(newBoard);
-          //console.log('next');
+          console.log('next');
           for (let x2 = 0; x2 < 8; x2++) {
             for (let y2 = 0; y2 < 8; y2++) {
-              //console.log(x2, y2); //
+              console.log(x2, y2); //
               for (const n of direction) {
-                //console.log(n);
+                console.log(n);
                 for (
                   let distance = 1;
                   distance <=
@@ -92,24 +92,25 @@ const Home = () => {
                 ) {
                   if (newBoard[y2][x2] === 3 - turnColor) {
                     if (newBoard[y2 + n[1] * distance][x2 + n[0] * distance] === turnColor) {
-                      //console.log('b');
+                      console.log('b');
                       continue;
                     } else if (newBoard[y2 + n[1] * distance][x2 + n[0] * distance] % 3 === 0) {
-                      //console.log('c', distance);
+                      console.log('c', distance);
                       for (let i = 1; i < distance; i++) {
-                        //console.log('d', [x2 + n[0] * distance], [y2 + n[1] * distance]);
+                        console.log('d', [x2 + n[0] * distance], [y2 + n[1] * distance]);
                         newBoard[y2 + n[1] * distance][x2 + n[0] * distance] = 3;
                       }
                       isPredict = true;
-                      //console.log(isPlace);
+                      console.log(isPlace);
                       break;
                     } else {
+                      isPredict = false;
                       break;
                     }
                   }
                 }
               }
-              ////console.table(newBoard);
+              //console.table(newBoard);
               setBoard(newBoard);
             }
           }
@@ -149,7 +150,7 @@ const Home = () => {
             <div className={styles.masu} key={`${x}-${y}`} onClick={() => clickMasu(x, y)}>
               {masu !== 0 && (
                 <div
-                  className={styles.stone}
+                  className={styles.ishi}
                   style={{
                     background: masu === 1 ? '#000' : masu === 2 ? '#fff' : '#ff6a00',
                     width: masu === 3 ? '20%' : '87.5%',
@@ -166,7 +167,7 @@ const Home = () => {
           {`${turnColor === 1 ? '黒の番' : '白の番'}` +
             `：` +
             `${board.some((row) => row.includes(3)) ? '置けます' : 'パス'}` +
-            `　`}
+            `  `}
         </h1>
       </div>
       <div className={styles.count}>
