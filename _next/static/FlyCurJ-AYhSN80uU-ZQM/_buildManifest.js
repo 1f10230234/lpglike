7c73cbac05763b4cc9832606dca8fd8551b182bc
1f10230234 <1f10230234@users.noreply.github.com>
2023-05-03 08:39:13 +0000

    deploy: c24c3d2b7f1044143a065efcc5ed7dcd9c791f22

---
+++ _next/static/FlyCurJ-AYhSN80uU-ZQM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4b676378455309d6.css","static/chunks/pages/index-1f9391d0f2c43d06.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
