7e0d6c92d3e0874bba6dc9ee3df3b81f756044e9
ito yossuli <sif102302348@iniad.org>
2023-05-04 22:13:43 +0900

    オセロver.2.0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  function refleshName() {
+  function refreshGame() {
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 8; j++) {
         newBoard[i][j] = normalBoard[i][j];
@@ -41,44 +41,7 @@ const Home = () => {
     const isFrontColor = (color: number, n: number[], distance: number, x: number, y: number) => {
       return board[y + n[1] * distance][x + n[0] * distance] % 3 === color;
     };
-    if (board.some((row) => row.includes(3))) {
-      if (board[y][x] === 3) {
-        isPlace = false;
-        for (const n of direction) {
-          for (
-            let distance = 1;
-            distance <=
-            Math.min(
-              Math.abs(3.5 + 3.5 * n[0] - x) + 99 * (1 - Math.abs(n[0])),
-              Math.abs(3.5 + 3.5 * n[1] - y) + 99 * (1 - Math.abs(n[1]))
-            );
-            distance++
-          ) {
-            if (isFrontColor(0, n, distance, x, y)) {
-              distance = 0;
-              break;
-            } else if (isFrontColor(3 - turnColor, n, distance, x, y)) {
-              continue;
-            } else if (isFrontColor(turnColor, n, distance, x, y)) {
-              for (let i = 1; i < distance; i++) {
-                newBoard[y + n[1] * (distance - i)][x + n[0] * (distance - i)] = turnColor;
-                isPlace = true;
-              }
-              break;
-            }
-          }
-        }
-      }
-    }
-    if (isPlace) {
-      newBoard[y][x] = turnColor;
-    }
-    if (isPlace || board.some((row) => row.includes(3)) === false) {
-      for (let x1 = 0; x1 < 8; x1++) {
-        for (let y1 = 0; y1 < 8; y1++) {
-          newBoard[y1][x1] %= 3;
-        }
-      }
+    function checkStone() {
       for (let x2 = 0; x2 < 8; x2++) {
         for (let y2 = 0; y2 < 8; y2++) {
           for (const n of direction) {
@@ -107,33 +70,70 @@ const Home = () => {
           }
         }
       }
+      console.log('a');
     }
-    if (newBoard.some((row) => row.includes(3))) {
-      setTurnColor(3 - turnColor);
-      setBoard(newBoard);
-    }
-    console.log(isPlace);
-    console.log(newBoard.some((row) => row.includes(3)));
-    if (isPlace) {
-      setTurnColor(3 - turnColor);
-      setBoard(newBoard);
-    } else {
-      if (newBoard.some((row) => row.includes(3))) {
+    if (board[y][x] === 3) {
+      isPlace = false;
+      for (const n of direction) {
+        for (
+          let distance = 1;
+          distance <=
+          Math.min(
+            Math.abs(3.5 + 3.5 * n[0] - x) + 99 * (1 - Math.abs(n[0])),
+            Math.abs(3.5 + 3.5 * n[1] - y) + 99 * (1 - Math.abs(n[1]))
+          );
+          distance++
+        ) {
+          if (isFrontColor(0, n, distance, x, y)) {
+            distance = 0;
+            break;
+          } else if (isFrontColor(3 - turnColor, n, distance, x, y)) {
+            continue;
+          } else if (isFrontColor(turnColor, n, distance, x, y)) {
+            for (let i = 1; i < distance; i++) {
+              newBoard[y + n[1] * (distance - i)][x + n[0] * (distance - i)] = turnColor;
+              isPlace = true;
+            }
+            break;
+          }
+        }
+      }
+      if (isPlace) {
+        newBoard[y][x] = turnColor;
+        for (let x1 = 0; x1 < 8; x1++) {
+          for (let y1 = 0; y1 < 8; y1++) {
+            newBoard[y1][x1] %= 3;
+          }
+        }
+        console.log('i');
+        checkStone();
         setTurnColor(3 - turnColor);
         setBoard(newBoard);
+      }
+    } else {
+      if (newBoard.some((row) => row.includes(3))) {
+        //
       } else {
-        if (count[0] !== count[1]) {
-          alert(`${count[0] > count[1] ? '黒' : '白'}の勝ち`);
-        }
-        if (confirm('新しいゲームを作りますか？')) {
-          refleshName;
-          setTurnColor(1);
-          setBoard(normalBoard);
-          isPlace = true;
-          console.log('i');
+        checkStone();
+        setTurnColor(3 - turnColor);
+        setBoard(newBoard);
+        if (newBoard.some((row) => row.includes(3)) === false) {
+          if (count[0] !== count[1]) {
+            alert(`${count[0] > count[1] ? '黒' : '白'}の勝ち`);
+          }
+          if (confirm('新しいゲームを作りますか？')) {
+            refreshGame;
+            setTurnColor(1);
+            setBoard(normalBoard);
+            isPlace = true;
+            console.log('i');
+          }
         }
       }
     }
+
+    console.log(isPlace);
+    console.log(newBoard.some((row) => row.includes(3)));
   };
   for (let k = 0; k < 2; k++) {
     let a = 0;
@@ -171,11 +171,7 @@ const Home = () => {
         <h1>
           {`${turnColor === 1 ? '黒' : '白'}` +
             `の番：置けま` +
-            `${
-              board.some((row) => row.includes(3))
-                ? 'す'
-                : 'せん。画面をクリックしてパスしてください'
-            }`}
+            `${board.some((row) => row.includes(3)) ? 'す' : 'せん。(画面をクリックしてパス)'}`}
         </h1>
       }
       <h1>{`白：` + `${count[0]}` + `個` + ` / ` + `黒：` + `${count[1]}` + `個`}</h1>
@@ -184,7 +180,7 @@ const Home = () => {
         onClick={() => {
           console.log('a');
           isPlace = true;
-          refleshName;
+          refreshGame;
           setBoard(normalBoard);
           setTurnColor(1);
         }}
